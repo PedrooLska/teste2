771d14d1d32dce17827c1d1406cc00700893b39c
PedrooLska <pedrolucas81@hotmail.com>
2023-01-17 17:10:36 -0300

    Add edit task

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="app">
     <div class="card-task">
-      <input class="card-task-input" type="text" v-model="newTask" @keypress.enter="setNewTask">
+      <input class="card-task-input" type="text" v-model="newTask.name" @keypress.enter="setTask">
+
+      {{ newTask }}
 
       <div class="task" v-for="(task, index) in listTasks" :key="task.id">
         <input class="form-check-input" type="checkbox" @click="checkTask(index, task)">
@@ -9,6 +11,13 @@
         <div class="task-text" :class="{ 'texto-riscado': task.checked }">
           {{ task.name }}
         </div>
+
+        <div @click="editTask(index, task)">
+          ✏️
+        </div>
+
+
+
       </div>
     </div>
   </div>
@@ -20,26 +29,64 @@ export default {
 
   data() {
     return {
-      newTask: '',
+      newTask: this.initialNewTask(),
       listTasks: []
     }
   },
 
   methods: {
-    setNewTask() {
+    initialNewTask() {
+      return { id: null, name: '', checked: false , indexEditing: null, isEditing: false }
+    },
+
+    setTaskEditing({ id, name, checked, indexEditing }) {
+      const newListTask = [...this.listTasks]
+      newListTask.splice(indexEditing, 1, { id, name, checked  })
+      this.listTasks = newListTask
+
+      this.newTask = this.initialNewTask()
+    },
+
+    setNewTask(name) {
       const newId = this.listTasks.length === 0 ? 0 : [...this.listTasks].pop().id + 1
 
       this.listTasks.push({
         id: newId,
-        name: this.newTask,
+        name,
         checked: false
       })
+
+      this.newTask = this.initialNewTask()
+    },
+
+    setTask() {
+      const { id, name, indexEditing, isEditing, checked } = this.newTask
+
+      if(name) {
+        if(isEditing) {
+          this.setTaskEditing({ id, name, checked, indexEditing })
+          return
+        }
+
+        this.setNewTask(name)
+        return 
+      }
+
+      alert('Por favor, preencha o campo para inserir a sua tarefa.')
     },
 
     checkTask(index, task) {
       const newListTask = [...this.listTasks]
       newListTask.splice(index, 1, { ...task, checked: !task.checked })
       this.listTasks = newListTask
+    },
+
+    editTask(index, task) {
+      this.newTask = {
+        ...task,
+        indexEditing: index,
+        isEditing: true
+      }
     }
   }
 }
