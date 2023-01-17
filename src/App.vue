2de69604dd292df1c7a487056ef3cf3a745de9d3
PedrooLska <pedrolucas81@hotmail.com>
2023-01-17 16:29:41 -0300

    Add input and list tasks

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,75 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="app">
+    <div class="card-task">
+      <input class="card-task-input" type="text" v-model="newTask" @keypress.enter="setNewTask">
+
+      <div class="task" v-for="(task, index) in listTasks" :key="task.id">
+        <input class="form-check-input" type="checkbox" @click="checkTask(index, task)">
+
+        <div class="task-text" :class="{ 'texto-riscado': task.checked }">
+          {{ task.name }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
 
-nav {
-  padding: 30px;
+  data() {
+    return {
+      newTask: '',
+      listTasks: []
+    }
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  methods: {
+    setNewTask() {
+      const newId = this.listTasks.length === 0 ? 0 : [...this.listTasks].pop().id + 1
 
-    &.router-link-exact-active {
-      color: #42b983;
+      this.listTasks.push({
+        id: newId,
+        name: this.newTask,
+        checked: false
+      })
+    },
+
+    checkTask(index, task) {
+      const newListTask = [...this.listTasks]
+      newListTask.splice(index, 1, { ...task, checked: !task.checked })
+      this.listTasks = newListTask
     }
   }
+}
+</script>
+
+<style lang="scss">
+.app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 800px;
+}
+
+.card-task {
+  width: 500px;
+}
+
+.card-task-input {
+  width: 100%;
+}
+
+.task {
+  display: flex;
+}
+
+.task-text {
+  margin-left: 10px;
+}
+
+.texto-riscado {
+  text-decoration: line-through;
 }
 </style>
